d136ef2e4fc5c8ffdb14c4a6fc9f19c9e3c7b7a8
tmowes <t-mowes@hotmail.com>
2020-04-09 19:11:22 -0300

    Projects CRD ReactJS frontEnd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 
-import "./styles.css";
+import api from './services/api'
 
-function App() {
+import './styles.css'
+
+export default function App() {
+  const [repositories, setRepositories] = useState([])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Desafio React.js',
+      url: 'http://github.com/tmowes/desafio-conceitos-reactjs',
+      techs: 'NodeJS, ReactJS, ReactNative',
+    })
+    const repository = response.data
+    setRepositories([...repositories, repository])
   }
+  // DONE
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data)
+    })
+  }, [])
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter((repository) => repository.id !== id))
   }
+  // DONE
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid='repository-list'>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
-
-export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,66 +1,66 @@
-import React from "react";
-import { render, fireEvent, act } from "@testing-library/react";
-import MockAdapter from "axios-mock-adapter";
-import api from "../services/api";
+import React from 'react'
+import { render, fireEvent, act } from '@testing-library/react'
+import MockAdapter from 'axios-mock-adapter'
+import api from '../services/api'
 
-const apiMock = new MockAdapter(api);
+const apiMock = new MockAdapter(api)
 
-import App from "../App";
+import App from '../App'
 
 const wait = (amount = 0) => {
-  return new Promise((resolve) => setTimeout(resolve, amount));
-};
+  return new Promise((resolve) => setTimeout(resolve, amount))
+}
 
 const actWait = async (amount = 0) => {
   await act(async () => {
-    await wait(amount);
-  });
-};
+    await wait(amount)
+  })
+}
 
-describe("App component", () => {
-  it("should be able to add new repository", async () => {
-    const { getByText, getByTestId } = render(<App />);
+describe('App component', () => {
+  it('should be able to add new repository', async () => {
+    const { getByText, getByTestId } = render(<App />)
 
-    apiMock.onGet("repositories").reply(200, []);
+    apiMock.onGet('repositories').reply(200, [])
 
-    apiMock.onPost("repositories").reply(200, {
-      id: "123",
-      url: "https://github.com/josepholiveira",
-      title: "Desafio ReactJS",
-      techs: ["React", "Node.js"],
-    });
+    apiMock.onPost('repositories').reply(200, {
+      id: '123',
+      url: 'https://github.com/josepholiveira',
+      title: 'Desafio ReactJS',
+      techs: ['React', 'Node.js'],
+    })
 
-    await actWait();
+    await actWait()
 
-    fireEvent.click(getByText("Adicionar"));
+    fireEvent.click(getByText('Adicionar'))
 
-    await actWait();
+    await actWait()
 
-    expect(getByTestId("repository-list")).toContainElement(
-      getByText("Desafio ReactJS")
-    );
-  });
+    expect(getByTestId('repository-list')).toContainElement(
+      getByText('Desafio ReactJS'),
+    )
+  })
 
-  it("should be able to remove repository", async () => {
-    const { getByText, getByTestId } = render(<App />);
+  it('should be able to remove repository', async () => {
+    const { getByText, getByTestId } = render(<App />)
 
-    apiMock.onGet("repositories").reply(200, [
+    apiMock.onGet('repositories').reply(200, [
       {
-        id: "123",
-        url: "https://github.com/josepholiveira",
-        title: "Desafio ReactJS",
-        techs: ["React", "Node.js"],
+        id: '123',
+        url: 'https://github.com/josepholiveira',
+        title: 'Desafio ReactJS',
+        techs: ['React', 'Node.js'],
       },
-    ]);
+    ])
 
-    apiMock.onDelete("repositories/123").reply(204);
+    apiMock.onDelete('repositories/123').reply(204)
 
-    await actWait();
+    await actWait()
 
-    fireEvent.click(getByText("Remover"));
+    fireEvent.click(getByText('Remover'))
 
-    await actWait();
+    await actWait()
 
-    expect(getByTestId("repository-list")).toBeEmpty();
-  });
-});
+    expect(getByTestId('repository-list')).toBeEmpty()
+  })
+})
